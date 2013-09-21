c6e7ae856a51a760c5f45181387091e1475eed71
Nicholas P Smith <npsmith1990@gmail.com>
2013-09-21 16:15:16 -0500

    Timer Implemented in Javascript using setInterval method.  Tick every 20 seconds
    [Closes #10]

--- public/application.js
+++ public/application.js
@@ -1,3 +1,59 @@
+
+
+// A view for the individual list items
+var itemView = Backbone.View.extend({
+  className: 'item',
+
+  events: {
+    // @ TODO
+  },
+
+  initialize: function (options) {
+    this.name = options.name;
+  },
+
+  render: function() {
+    var newItemHtml = commentTemplate({
+      
+    });
+  }
+});
+
+
+// A view for the list (collection) of items
+var list = Backbone.View.extend({
+  initialize: function () {
+    this.itemViews = [];
+
+  },
+
+  addItem: function (){
+
+  }
+});
+
+
+
+
+
+
+
+
+
+
+var checkTime = function () {
+  // TODO check to see if time is up...
+}
+
+// Call every 20 seconds
 window.setInterval(function(){
-  // @TODO check to see if time is up...
-}, 30000);
+  console.log('tick');
+  checkTime();
+}, 20000);
+
+
+// Call when list item is submitted
+$(document).on('submit', function (e) {
+  e.preventDefault();
+  console.log();
+});
