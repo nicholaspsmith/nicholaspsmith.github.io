f5e85dd7701d6bbd39c14de8927949f5cbe17085
Nicholas P Smith <npsmith1990@gmail.com>
2013-09-22 00:43:07 -0500

    Change style to Mission Impossible theme

--- public/application.js
+++ public/application.js
@@ -2,9 +2,11 @@
 var firebase = new Firebase('https://todonkey.firebaseio.com/');
 
 $('#task').keypress(function (e){
+
   var input = $('#task').val();
 
   if(jQuery.trim(input).length > 0 && e.keyCode == 13){
+    e.preventDefault();
 
     var item = $('#task').val();
     console.log(item);
@@ -26,5 +28,5 @@ firebase.on('child_added', function(snapshot){
 // Display a task
 var displayNewTask = function (name, complete){
   //$('#todo-list').prependTo()
-  $('<div class="item"/>').text(name).prependTo($('#todo-list'));
+  $('<div class="item"/>').text(name, complete).prependTo($('#todo-list'));
 };
