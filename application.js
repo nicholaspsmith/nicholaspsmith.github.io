a928be6ce205df00c30679f27ad6a6e5e11be57c
Nicholas Smith <me@nicholaspsmith.com>
2013-09-25 11:27:45 -0500

    Update application.js

--- application.js
+++ application.js
@@ -204,7 +204,7 @@ firebaseDue.on('child_changed', function () {
 var firebaseAuth = new Firebase('https://todonkey.firebaseio.com/');
 var auth = new FirebaseSimpleLogin(firebaseAuth, function(error, user) {
     if (error) {
-        console.log("Login error: ", error);
+        console.log("Login error: ", error.code);
     } else if (user) {
         alert("Logged in as ", user.id);
     } else {
